1b913ce2ba67a921ee701e9c1adfb90dc87e9f82
Mateusz Mikołaj Okulus <m.okulus@stud.elka.pw.edu.pl>
2023-05-25 16:20:35 +0200

    Add VL53L0X library

--- projekt/server/src/main.cpp
+++ projekt/server/src/main.cpp
@@ -2,9 +2,17 @@
 #include <chrono>
 #include <thread>
 #include "Server.h"
-
+extern "C" {
+    #include "tof.h"
+}
 
 int main() {
+    int handle = tofInit(0, 0, 1);
+    if (handle != 1)
+	{
+        std::cerr << "VL53L0X init error" << std::endl;
+		return -1;
+	}
     Server server;
     while (true) {
         std::cout << server.client_count() << " clients" << std::endl;
