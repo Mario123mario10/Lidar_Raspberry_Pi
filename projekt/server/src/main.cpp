8ea3fb59c53781a1e96db59179616723e497bead
Mateusz Mikołaj Okulus <m.okulus@stud.elka.pw.edu.pl>
2023-05-25 19:54:11 +0200

    Fixes

--- projekt/server/src/main.cpp
+++ projekt/server/src/main.cpp
@@ -7,12 +7,20 @@ extern "C" {
 }
 
 int main() {
-    int handle = tofInit(0, 0, 1);
+    int handle = tofInit(1, 0x29, 1);
     if (handle != 1)
 	{
         std::cerr << "VL53L0X init error" << std::endl;
 		return -1;
-	}
+	} else {
+        int model, revision;
+        tofGetModel(&model, &revision);
+        std::cout << "model " << model << " revision " << revision << std::endl;
+        while (true) {
+            std::cout << tofReadDistance() << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+    }
     Server server;
     while (true) {
         std::cout << server.client_count() << " clients" << std::endl;
