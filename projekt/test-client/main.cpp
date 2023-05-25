c876a7aa7757be5e66d5c8a2e9e4ed4aa741db9d
Mateusz Mikołaj Okulus <m.okulus@stud.elka.pw.edu.pl>
2023-05-25 16:19:00 +0200

    Change client

--- projekt/test-client/main.cpp
+++ projekt/test-client/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     Client client;
-    for (size_t i = 0; i < 10; ++i) {
+    for (size_t i = 0; i < 100; ++i) {
         std::cout << std::setw(2) << i << "  received: " << client.read() << std::endl;
     }
 }
